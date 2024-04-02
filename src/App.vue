357f04a172f6d5b494ea79259a77fb19339e427a
Gema Dodi Pranata <gemadp01@gmail.com>
2024-04-02 23:19:05 +0700

    add: Membuat component price, membuat component produc list, membuat method add item, membuat price slider component (belum berfungsi, tidak singkon dengan parent property)

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,55 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="app" class="container mt-5">
+    <h1>IDShop</h1>
+    <price-slider :sliderStatus="sliderStatus" @update:maximum="maximum"></price-slider>
+    <product-list :products="products" :maximum="maximum" @add="addItem"></product-list>
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import PriceSlider from "./components/PriceSlider.vue";
+import ProductList from "./components/ProductList.vue";
 
 export default {
-  name: "App",
+  name: "app",
   components: {
-    HelloWorld,
+    PriceSlider,
+    ProductList,
+  },
+  data: function () {
+    return {
+      maximum: 50,
+      products: [],
+      cart: [],
+      sliderStatus: true,
+    };
+  },
+  mounted: function () {
+    fetch("https://hplussport.com/api/products/order/price?limit=10")
+      .then((response) => response.json())
+      .then((data) => {
+        this.products = data;
+      });
+  },
+  methods: {
+    addItem: function (product) {
+      // this.cart.push(item);
+      let productIndex;
+      let productExist = this.cart.filter(function (item, index) {
+        if (item.product.id == Number(product.id)) {
+          productIndex = index;
+          return true;
+        } else {
+          return false;
+        }
+      });
+
+      if (productExist.length) {
+        this.cart[productIndex].qty++;
+      } else {
+        this.cart.push({ product: product, qty: 1 });
+      }
+    },
   },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+import "animate.css/animate.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 
-createApp(App).mount("#app");
+library.add(faCartShopping);
+
+const app = createApp(App);
+
+app.component("font-awesome-icon", FontAwesomeIcon);
+
+app.mount("#app");
